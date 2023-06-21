6395e6b9a21ac7028d8659ea4f5a0373603f9fe0
Ashish Kumar Mahuri <ashishmahuri85@gmail.com>
2023-06-21 16:58:48 +0200

    Splitted the navigation into components and used event emitters.

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent {
+
+  @Output() closeSideNav = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onClose() {
+    this.closeSideNav.emit();
+  }
+}
